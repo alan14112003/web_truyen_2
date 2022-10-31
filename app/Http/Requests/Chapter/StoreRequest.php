df20d87948c10c030437ce586a798afd6d3540f3
alan14112003 <sonnn.21it@gmil.com>
2022-10-31 12:01:58 +0700

    Add all admin page

--- app/Http/Requests/Chapter/StoreRequest.php
+++ app/Http/Requests/Chapter/StoreRequest.php
@@ -1,10 +1,12 @@
 <?php
 
-namespace App\Http\Requests;
+namespace App\Http\Requests\Chapter;
 
+use App\Models\Story;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
-class UpdateCategoryRequest extends FormRequest
+class StoreRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -13,7 +15,7 @@ class UpdateCategoryRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +26,12 @@ class UpdateCategoryRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'story_id' => [
+                'required',
+                Rule::exists(Story::class, 'id'),
+            ],
+            'name' => 'required',
+            'content' => 'required',
         ];
     }
 }
